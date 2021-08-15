ec5450a6520f0e7e8c7a42be91664a987b91c49a
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-16 03:09:55 +0530

    Added users and posts controllers

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,4 @@
+// controller for home page
 module.exports.home = function (req, res) {
-    return res.end('<h1> Home </h1>')
-}
-
-module.exports.profile = (req, res) => {
-    return res.end('<h1> Profile </h1>')
+    return res.end('<h1> Home </h1>');
 }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
 
+// router for home page
 router.get('/', homeController.home);
 
-router.use('/profile', homeController.profile);
+// router for users page
+router.use('/users', require('./users'));
+
+//router for posts
+router.get('/posts', require('./posts'));
+
+// router.use('/routerName', require('./routerFile));
 
 module.exports = router;
