c8f96175ae229b744ce2067590b0cfd1ff8b9afc
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-16 03:25:50 +0530

    Setup EJS View Engine

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 // using express router
 app.use('/', require('./routes'));
 
+// setup EJS View Engine
+app.set('view engine', 'ejs');
+app.set('views', './views')
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running server: ${err}`)
