aa19617538eb93dd07e22fda64ea2e3b58ede657
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-17 22:19:49 +0530

    Added EJS layouts

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const app = express();
 const port = 8000;
+
+// using express EJS Layouts
+app.use(expressLayouts);
 
 // using express router
 app.use('/', require('./routes'));
@@ -13,6 +17,5 @@ app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running server: ${err}`)
     }
-
     console.log(`Server running on port ${port}`)
 });
