4cc0a6f4c77f6c7788cbdc0be0142da910d5e79e
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-18 21:41:51 +0530

    Added manual signup authentication

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 // rendering profile page
 module.exports.profile = function (req, res) {
     return res.render('users', {
@@ -16,5 +18,33 @@ module.exports.signUp = function (req, res) {
 module.exports.signIn = function (req, res) {
     return res.render('sign_in', {
         title: "Sign-in"
+    });
+}
+
+// getting the sign up data
+module.exports.create = function (req, res) {
+
+    // if confirm password does not match
+    if (req.body.password != req.body.confirm_password) { return res.redirect('back'); }
+
+    User.findOne({ email: req.body.email }, function (err, user) {
+        if (err) {
+            console.log('Error in finding user in signing up');
+            return;
+        }
+
+        if (!user) {
+            User.create(req.body, function (err, user) {
+                if (err) { console.log('Error in creating user in signing up'); return }
+
+                return res.redirect('/users/sign-in');
+            })
+
+        }
+
+        else {
+            return res.redirect('back');
+        }
+
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
+const User = require('../models/user');
+
+// profile router
+router.get('/profile', usersController.profile);
 
 // sign up router
 router.get('/sign-up', usersController.signUp);
@@ -9,7 +13,7 @@ router.get('/sign-up', usersController.signUp);
 // sign in router
 router.get('/sign-in', usersController.signIn);
 
-// profile router
-router.get('/profile', usersController.profile);
+// create user router
+router.post('/create', usersController.create);
 
 module.exports = router;
