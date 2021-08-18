766cf42b0bc0faa7a5f504d617d1fe7c11f36d9f
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-18 18:54:20 +0530

    Added sign-up and sign-in pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,20 @@
+// rendering profile page
 module.exports.profile = function (req, res) {
     return res.render('users', {
         title: "Profile"
+    });
+}
+
+// rendering sign-up page
+module.exports.signUp = function (req, res) {
+    return res.render('sign_up', {
+        title: "Sign-up"
+    });
+}
+
+// rendering sign-in page
+module.exports.signIn = function (req, res) {
+    return res.render('sign_in', {
+        title: "Sign-in"
     });
 }
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const usersController = require('../controllers/users_controller');
 
+// sign up router
+router.get('/sign-up', usersController.signUp);
+
+// sign in router
+router.get('/sign-in', usersController.signIn);
+
+// profile router
 router.get('/profile', usersController.profile);
 
 module.exports = router;
