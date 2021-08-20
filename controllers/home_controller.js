53c3dd590900758737adb2f1a2f6bc44ec4213af
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-08-21 02:56:11 +0530

    Displaying posts on home page aswell as the user who posted it

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,6 +1,21 @@
+const Posts = require('../models/post');
+
 // controller for home page
 module.exports.home = function (req, res) {
-    return res.render('home', {
-        title: "Home"
+    // Posts.find({}, function (err, posts) {
+    //     return res.render('home', {
+    //         title: "Clouet | Home",
+    //         posts: posts
+    //     });
+    // });
+
+
+    // populate the user of each post
+    Posts.find({}).populate('user').exec(function (err, posts) {
+        return res.render('home', {
+            title: "Clouet | Home",
+            posts: posts
+        });
     });
+
 }
