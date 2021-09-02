c770a5202cc5a36b03f4dd25175eb529e5ab2834
anujsharma7996 <sharma.anuj7996@gmail.com>
2021-09-02 23:34:44 +0530

    Setup JWT

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,47 @@
-module.exports.index = function (req, res) {
+const { findById } = require('../../../models/post');
+const Posts = require('../../../models/post');
+const Comment = require('../../../models/comment');
+
+module.exports.index = async function (req, res) {
+
+    let posts = await Posts.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
     return res.json(200, {
-        message: "List of posts",
-        posts: []
+        message: "List of posts v1",
+        posts: posts
     })
+}
+
+module.exports.destroy = async function (req, res) {
+    try {
+        let post = await Posts.findById(req.params.id);
+
+        // if (post.user = req.user.id) {
+
+        post.remove();
+
+        await Comment.deleteMany({ post: req.params.id });
+
+        return res.json(200, {
+            message: "Post has been deleted"
+        });
+
+        // else {
+        // return res.redirect('back');
+        // }
+
+    } catch (err) {
+        console.log(err);
+        return res.json(200, {
+            message: "Internal Server Error"
+        })
+    }
 }
--- controllers/api/v2/posts_api.js
+++ controllers/api/v2/posts_api.js
@@ -1,6 +1,20 @@
-module.exports.index = function (req, res) {
+const Posts = require('../../../models/post');
+
+module.exports.index = async function (req, res) {
+
+    let posts = await Posts.find({})
+        .sort('-createdAt')
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        });
+
+
     return res.json(200, {
         message: "List of posts v2",
-        posts: []
+        posts: posts
     })
 }
